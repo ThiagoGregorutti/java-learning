ccca93368f84716d1fa2717ca733febed9011586
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-03 00:10:17 -0300

    Conditionals exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        // Declaring variables
-        String MyName = "Gioh";
-        int MyAge = 20;
-        double MyHeight = 1.80;
+        // Primitive variables
+        int temperature = 10;
+        boolean raining = true;
+        int hour = 12;
 
-        System.out.println("My name is " + MyName + ", I'm " + MyAge + " years old " + " and " + MyHeight + " meters tall.");
+        // Hour of the day
+        if (hour < 12) {
+            System.out.println("Good morning!");
+        }
+        else if (hour == 12) {
+            System.out.println("Good noon!");
+        }
+        else if (hour < 18) {
+            System.out.println("Good afternoon!");
+        }
+        else {
+            System.out.println("Good evening!");
+        }
 
+        // Temperature conditions
+        if (temperature < 0) {
+            System.out.println("It's freezing.");
+        } else if (temperature < 15) {
+            System.out.println("It's cold.");
+        } else if (temperature < 25) {
+            System.out.println("It's warm.");
+        } else {
+            System.out.println("It's hot.");
+        }
+
+        // Raining condition
+        if (raining) {
+            System.out.println("Don't forget your umbrella.");
+        }
     }
 }
+
