2ccd5e95dc93ea541017780e7b7dc17b73f3dd77
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-02 22:25:17 -0300

    Declaring Variables

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declaring variables
+        String MyName = "Gioh";
+        int MyAge = 20;
+        double MyHeight = 1.80;
+
+        System.out.println("My name is " + MyName + ", I'm " + MyAge + " years old " + " and " + MyHeight + " meters tall.");
+
+
+
+
+    }
+}
