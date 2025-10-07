375e3b66859062c9046671eb14200593f5b2ce94
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-07 01:17:08 -0300

    Conditionals exercise.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,29 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 0;
+        boolean messiScores = true;
+        int goalCount = 0;
 
-        while (i <= 1000) {
-            System.out.println(i);
-            i++;
+        while(messiScores) {
+            goalCount++;
+            System.out.println("Messi goals count: " + goalCount);
+            if (goalCount == 3) {
+                messiScores = false;
+            }
         }
-
 
     }
 }
+
+/* int[] numbers = {10, 20, 30};
+
+        for (int i = 0; i < numbers.length; i++) {
+        System.out.println(numbers[i]);
+
+########################################################
+
+            for (int i = 0; i <= 5; i++) {
+        System.out.println(i);
+
+
+ */
