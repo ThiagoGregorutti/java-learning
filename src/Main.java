b2a3100c2947418cb5e5c1a43ffaa9e68a2306cd
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-10 23:01:25 -0300

    Loops exercises using for and while

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
 
         int i = 10;
         while ( i >= 1){
-            System.out.println(i);
+            System.out.println(i) ;
             i--;
         }
     }
