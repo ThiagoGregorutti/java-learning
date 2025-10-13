57039242cbff4e9f3f0c2d7b856b80143f9669b7
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-12 22:32:31 -0300

    Loops exercises using for and while

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         }
 
         int i = 10;
-        while ( i >= 1){
-            System.out.println(i) ;
+        while ( i >= 1 {
+            System.out.println(i);
             i--;
         }
     }
