5d219610b7bdd47074259b1f7a81905109608d63
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-10-13 20:07:25 -0300

    Beginning of OOP.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
+        // Create two Dog objects
+        Player player1 = new Player();
+        Player player2 = new Player();
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+        // Set their attributes
+        player1.name = "Apollo";
+        player1.score = 888;
 
-        int i = 10;
-        while ( i >= 1 {
-            System.out.println(i);
-            i--;
-        }
+        player2.name = "Dionysus";
+        player2.score = 666;
+
+        // Make them bark
+        player1.showScore();
+        player2.showScore();
+    }
+}
+
+
+class Player {
+    String name;
+    int score;
+
+    void showScore() {
+        System.out.println(name + " has a score of " + score);
     }
 }
