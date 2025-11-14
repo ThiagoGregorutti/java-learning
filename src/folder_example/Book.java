6300a53512c5287fdad1a3eb863a7ac6316beefb
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-14 20:10:08 -0300

    nietzsche

--- src/folder_example/Book.java
+++ src/folder_example/Book.java
@@ -4,7 +4,20 @@ public class Book {
     String title;
     String author;
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
     void read() {
         System.out.println(" You are currently reading " + title + " by " + author + ".");
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
     }
 }
--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -2,15 +2,14 @@ package folder_example;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book();
-        book1.title = "The Odyssey";
-        book1.author = "Homer";
+        Book book1 = new Book("The Iliad", "Homer");
 
-        Book book2 = new Book();
-        book2.title = "The Joyous Science";
-        book2.author = "Nietzsche";
+        Book book2 = new Book("The Joyous Science", "Nietzsche") ;
 
         book1.read();
         book2.read();
+
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
