34f53674ad4dbce4bee2ad096390f4c073f40c8b
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-15 18:15:50 -0300

    user input

--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -1,15 +1,25 @@
 package folder_example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("The Iliad", "Homer");
 
-        Book book2 = new Book("The Joyous Science", "Nietzsche");
+        Scanner scanner = new Scanner(System.in);
 
-        book1.read();
-        book2.read();
+        double width = 0;
+        double height = 0;
+        double area = 0;
 
-        System.out.println(book1);
-        System.out.println(book2);
+        System.out.println("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.println("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The area is " + area + "cm^2");
+
+        scanner.close();
     }
 }
