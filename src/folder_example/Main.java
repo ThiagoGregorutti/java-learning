352442126944289bb308b0d3df26bd9a0ad9b587
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-16 19:27:39 -0300

    shopping cart program

--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -6,28 +6,22 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        String item;
+        double price = 99.99;
+        int quantity;
+        char currency = 'G';
+        double total;
 
-        System.out.println("Enter an adjective (description):");
-        adjective1 = scanner.nextLine();
-        System.out.println("Enter a noun (player)");
-        noun1 = scanner.nextLine();
-        System.out.println("Enter an adjective (description):");
-        adjective2 = scanner.nextLine();
-        System.out.println("Enter a verb end with -ing:");
-        verb1 = scanner.nextLine();
-        System.out.println("Enter an adjective (description):");
-        adjective3 = scanner.nextLine();
 
-        System.out.println("\nToday I joined a " + adjective1 + " party finder.");
-        System.out.println("There, I saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 + "!");
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
 
+        System.out.print("How many would you like?");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.print("\nThe total for " + quantity + " " + item + " would be " + total + currency);
 
         scanner.close();
     }
