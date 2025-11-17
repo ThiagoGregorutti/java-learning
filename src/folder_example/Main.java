2235846d8d54f60e8747e93194575c271cc2f3dd
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-17 15:45:22 -0300

    java util random

--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -1,53 +1,21 @@
 package folder_example;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        int level;
-        String job;
-        boolean isRaider;
+        boolean number;
 
-        System.out.print("What is your Job? ");
-        job = scanner.nextLine();
+        number = random.nextBoolean();
 
-        System.out.print("What is your level? ");
-        level = scanner.nextInt();
-
-        System.out.println("Are you a Raider? (true/false)");
-        isRaider = scanner.nextBoolean();
-
-
-        //job
-        if(job.isEmpty()){
-            System.out.print("You didn't enter your Job");
+        if (number){
+            System.out.println("Heads");
         }
         else{
-            System.out.print(job + " is a based choice!\n");
+            System.out.println("Tails");
         }
 
-        //level
-        if(level >= 100){
-            System.out.print("You can enter FRU ");
-        }
-        else if(level >= 80){
-            System.out.print("You can enter UCOB ");
-        }
-        else{
-            System.out.print("You must get at least level 80 before joining an Ultimate Raid");
-        }
-
-        //raider
-        if(isRaider){
-            System.out.print("You're a raider!");
-        }
-        else{
-            System.out.println("You're not a raider");
-        }
-
-
-        scanner.close();
     }
 }
