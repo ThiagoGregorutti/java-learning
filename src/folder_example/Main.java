34cf4a957ccf8d3846829c58c7ad7e5d0327d25c
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-17 15:21:19 -0300

    if statements

--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -6,22 +6,47 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String item;
-        double price = 99.99;
-        int quantity;
-        char currency = 'G';
-        double total;
+        int level;
+        String job;
+        boolean isRaider;
+
+        System.out.print("What is your Job? ");
+        job = scanner.nextLine();
+
+        System.out.print("What is your level? ");
+        level = scanner.nextInt();
+
+        System.out.println("Are you a Raider? (true/false)");
+        isRaider = scanner.nextBoolean();
 
 
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
+        //job
+        if(job.isEmpty()){
+            System.out.print("You didn't enter your Job");
+        }
+        else{
+            System.out.print(job + " is a based choice!\n");
+        }
 
-        System.out.print("How many would you like?");
-        quantity = scanner.nextInt();
+        //level
+        if(level >= 100){
+            System.out.print("You can enter FRU ");
+        }
+        else if(level >= 80){
+            System.out.print("You can enter UCOB ");
+        }
+        else{
+            System.out.print("You must get at least level 80 before joining an Ultimate Raid");
+        }
 
-        total = price * quantity;
+        //raider
+        if(isRaider){
+            System.out.print("You're a raider!");
+        }
+        else{
+            System.out.println("You're not a raider");
+        }
 
-        System.out.print("\nThe total for " + quantity + " " + item + " would be " + total + currency);
 
         scanner.close();
     }
