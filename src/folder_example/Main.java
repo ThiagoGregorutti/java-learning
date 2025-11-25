573843879656cc52a36dc701e114507a58342991
Thiago Gregorutti <142695912+ThiagoGregorutti@users.noreply.github.com>
2025-11-25 16:06:06 -0300

    width

--- src/folder_example/Main.java
+++ src/folder_example/Main.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double price1 = 9.99;
-        double price2 = 100.15;
-        double price3 = -54.01;
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
 
-        System.out.printf("%.1f\n", price1);
-        System.out.printf("%.2f\n", price2);
-        System.out.printf("%.3f\n", price3);
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%04d\n", id2);
+        System.out.printf("%04d\n", id3);
+        System.out.printf("%04d\n", id4);
+
     }
 }
